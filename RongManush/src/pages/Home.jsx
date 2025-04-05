5a7c74b1e016f3198ad456baa1fcbf73bad02a92
AYAN HALDAR <gopalchandrahaldar1997@gmail.com>
2025-04-05 09:43:25 +0530

    React Dom Router Added

---
+++ RongManush/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to RongManush</h1>
+      <p>Your go-to space for fashion, grooming & product tips for men.</p>
+    </div>
+  );
+}
+
+export default Home;
